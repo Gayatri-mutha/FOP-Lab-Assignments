d953921a907e238bf659937500f326b753c7a242
Gayatri-mutha <gayatrimutha69@gmail.com>
2026-04-14 22:23:11 +0530

    Add student details input and result display

---
+++ assignment7.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+struct student {
+    int roll;
+    char name[50];
+    float marks[3];
+    float total, percentage;
+};
+
+int main() {
+    struct student s[10];
+    int n, i, j;
+
+    printf("Enter number of students: ");
+    scanf("%d", &n);
+
+    for(i = 0; i < n; i++) {
+        printf("\nEnter details of student %d\n", i+1);
+
+        printf("Roll No: ");
+        scanf("%d", &s[i].roll);
+
+        printf("Name: ");
+        scanf("%s", s[i].name);
+
+        s[i].total = 0;
+
+        printf("Enter marks of 3 subjects:\n");
+        for(j = 0; j < 3; j++) {
+            scanf("%f", &s[i].marks[j]);
+            s[i].total += s[i].marks[j];
+        }
+
+        s[i].percentage = s[i].total / 3;
+    }
+
+    printf("\n--- Student Results ---\n");
+
+    for(i = 0; i < n; i++) {
+        printf("\nRoll No: %d", s[i].roll);
+        printf("\nName: %s", s[i].name);
+        printf("\nTotal: %.2f", s[i].total);
+        printf("\nPercentage: %.2f\n", s[i].percentage);
+    }
+
+    return 0;
+}
